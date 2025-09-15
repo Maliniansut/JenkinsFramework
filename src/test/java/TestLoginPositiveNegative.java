1d83af04ff1e940c4ddb09703a2b13c0bc70c27d
Maliniansut <maliniansu@gmail.com>
2025-09-15 11:46:26 +0530

    xml files

--- src/test/java/TestLoginPositiveNegative.java
+++ src/test/java/TestLoginPositiveNegative.java
@@ -9,7 +9,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestLoginPositiveNegative {
-    WebDriver driver;
+    WebDriver driver = new ChromeDriver();
 
     @BeforeMethod
     public void setup() {
